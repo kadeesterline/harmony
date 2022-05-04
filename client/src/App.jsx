1f56b0551f6a41cb256e0b45a4e5ce899588f764
Kade <kadeesterline@gmail.com>
2022-05-04 10:01:39 -0700

    changes routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -31,8 +31,8 @@ function App() {
           <Router>
             <Routes>
               <Route index element={<Home />}></Route>
-              <Route path="/login" element={<SignIn />} />
-              <Route path="/signup" element={<SignUp />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/newuser" element={<SignUp />} />
               <Route element={<NavLayout />}>
                 <Route path="/room/:id" element={<Room />} />
                 <Route path="/new" element={<New />} />
